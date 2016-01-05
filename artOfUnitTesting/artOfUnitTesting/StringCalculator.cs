d2bf1a37c8c7fddf191a5da2e341f054e31d513c
Mateusz Kopij <sagasu@gmail.com>
2016-01-05 10:10:54 +0000

    Initial StringCalculator kata.

---
+++ artOfUnitTesting/artOfUnitTesting/StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+//http://osherove.com/tdd-kata-1
+//Not allowed to use Regex :(
+namespace artOfUnitTesting
+{
+    public class StringCalculator
+    {
+        private const char DEFAULT_DELIMITER = ',';
+        private const char END_LINE_DELIMITER = '\n';
+        private const string CUSTOM_DELIMITER_PREFIX = @"//[";
+        private const string CUSTOM_DELIMITER_POSTFIX = @"]\n";
+
+        public int Add(string numbers)
+        {
+            if(string.IsNullOrWhiteSpace(numbers))
+                return 0;
+
+            numbers = ReplaceCustomDelimiterWithDefault(numbers);
+
+            var splitNumbers = numbers.Replace(END_LINE_DELIMITER,DEFAULT_DELIMITER).Split(DEFAULT_DELIMITER);
+
+            return splitNumbers.Sum(_ =>
+                {
+                    var parsedNumber = int.Parse(_);
+                    if(parsedNumber < 0)
+                        throw new Exception("Negatives not allowed");
+                    return parsedNumber;
+                });
+        }
+
+        private string ReplaceCustomDelimiterWithDefault(string numbers)
+        {
+            if (numbers.StartsWith(CUSTOM_DELIMITER_PREFIX))
+            {
+                var numbersWithRemovedPrefix = numbers.Replace(CUSTOM_DELIMITER_PREFIX, string.Empty);
+                var indexOfPostfix = numbersWithRemovedPrefix.IndexOf(CUSTOM_DELIMITER_POSTFIX, System.StringComparison.Ordinal);
+                var customDelimiter = numbersWithRemovedPrefix.Substring(0, indexOfPostfix);
+                return numbersWithRemovedPrefix.TrimStart(customDelimiter.ToCharArray()).Replace(customDelimiter, DEFAULT_DELIMITER.ToString()).Replace(CUSTOM_DELIMITER_POSTFIX, "");
+            }
+            return numbers;
+        }
+    }
+}
---
+++ artOfUnitTesting/artOfUnitTestingTests/StringCalculatorTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using NUnit.Framework;
+using artOfUnitTesting;
+
+//http://osherove.com/tdd-kata-1
+namespace artOfUnitTestingTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Add_EmptyString_ReturnDefaultValue()
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add("");
+
+            Assert.AreEqual(0, sum);
+        }
+        
+        [TestCase("1",1)]
+        [TestCase("2",2)]
+        public void Add_OneNumber_ReturnThatNumber(string number, int expectedSum)
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add(number);
+
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("3,4", 7)]
+        public void Add_TwoNumbers_ReturnSumOfThoseNumbers(string numbers, int expectedSum)
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add(numbers);
+
+            Assert.AreEqual(expectedSum, sum);
+        }
+        
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("1,2,3,5,7,11,13,17", 59)]
+        [TestCase("1000,100000", 101000)]
+        public void Add_MultipleNumbers_ReturnSumOfThoseNumbers(string numbers, int expectedSum)
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add(numbers);
+
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestCase("1\n2", 3)]
+        [TestCase("1\n2\n3", 6)]
+        [TestCase("1\n2,3", 6)]
+        [TestCase("1,2\n3", 6)]
+        public void Add_NumbersSeparatedByNewLineDelimiter_ReturnSumOfNumbers(string numbers, int expectedSum)
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add(numbers);
+
+            Assert.AreEqual(expectedSum, sum);
+        }
+
+        [TestCase(@"//[$]\n2", 2)]
+        [TestCase(@"//[$]\n2$3", 5)]
+        [TestCase(@"//[$]\n2$3$9", 14)]
+        [TestCase(@"//[$]\n2$3,9", 14)]
+        [TestCase(@"//[$]\n2,3$9", 14)]
+        public void Add_NumbersSeparatedByCustomDelimiter_ReturnSumOfNumbers(string numbers, int expectedSum)
+        {
+            var calculator = new StringCalculator();
+
+            var sum = calculator.Add(numbers);
+
+            Assert.AreEqual(expectedSum, sum);
+        }
+        
+        [TestCase(@"2,-3")]
+        public void Add_NegativeNumbersNotAllowed_ThrowsException(string numbers)
+        {
+            var calculator = new StringCalculator();
+
+            try
+            {
+                var sum = calculator.Add(numbers);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "Negatives not allowed");
+            }
+        }
+    }
+}
